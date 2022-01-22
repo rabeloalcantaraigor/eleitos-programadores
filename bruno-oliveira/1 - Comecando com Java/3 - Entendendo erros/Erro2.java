c59c6de6f19c32d360f92ef5db2fc5888ff800bd
Bruno Oliveira <brunno.oliver7@gmail.com>
2022-01-22 17:54:31 -0300

    'Comecando com Java' finalizado

---
+++ bruno-oliveira/1 - Comecando com Java/3 - Entendendo erros/Erro2.java
@@ -0,0 +1,6 @@
+// public class Erro2 {
+//   public static void main(String[] args) {
+//     int cont;
+//     System.out.println("Hello World" + cont);
+//   }
+// }
---
+++ bruno-oliveira/1 - Comecando com Java/3 - Entendendo erros/Erro3.java
@@ -0,0 +1,5 @@
+public class Erro3 {
+  public static void Main(String[] args) {
+    System.out.println("Hello World");
+  }
+}
