40faa9306c6cd16023bd543f16cd717768610cb4
Bruno Oliveira <brunno.oliver7@gmail.com>
2022-01-24 23:38:11 -0300

    Variaveis

---
+++ bruno-oliveira/2 - Programacao Basica/1 - Variaveis/Variaveis.java
@@ -0,0 +1,13 @@
+class Variaveis {
+  public static void main(String[] args) {
+    double doub = 2.9;
+    float flutuante = 2.9f;
+    int inteiro = 1;
+    long longo = 1L;
+
+    System.out.println(doub);
+    System.out.println(flutuante);
+    System.out.println(inteiro);
+    System.out.println(longo);
+  }
+}
