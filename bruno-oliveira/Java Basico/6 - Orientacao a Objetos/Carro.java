648cd290cbe36c091413180c6247fb9cd91a4d04
Bruno Oliveira <brunno.oliver7@gmail.com>
2022-01-26 19:47:28 -0300

    POO Metodo com retorno

--- bruno-oliveira/Java Basico/6 - Orientacao a Objetos/Carro.java
+++ bruno-oliveira/Java Basico/6 - Orientacao a Objetos/Carro.java
@@ -2,10 +2,15 @@ public class Carro {
   String marca;
   String modelo;
   int numPassageiros;
-	double capCombustivel;
-	double consumoCombustivel;
+  double capCombustivel;
+  double consumoCombustivel;
 
   void exibirAutonomia() {
     System.out.println("A autonomia do carro é: " + capCombustivel * consumoCombustivel + " km");
-}
+  }
+
+  double obterAutonomia() {
+    System.out.println("Método obterAutonomia foi chamado.");
+    return capCombustivel * consumoCombustivel;
+  }
 }
--- bruno-oliveira/Java Basico/6 - Orientacao a Objetos/TesteCarro.java
+++ bruno-oliveira/Java Basico/6 - Orientacao a Objetos/TesteCarro.java
@@ -1,26 +1,29 @@
 public class TesteCarro {
   public static void main(String[] args) {
-		
-		Carro van = new Carro();
-		van.marca = "Fiat";
-		van.modelo = "Ducato";
-		van.numPassageiros = 10;
-		van.capCombustivel = 100;
-		van.consumoCombustivel = 0.2;
-		
-		System.out.println(van.marca);
-		System.out.println(van.modelo);
-		
-		Carro fusca = new Carro();
-		fusca.marca = "Volkswagen";
-		fusca.modelo = "Fusca";
-		fusca.numPassageiros = 4;
-		fusca.capCombustivel = 30;
-		fusca.consumoCombustivel = 0.15;
-		
-		System.out.println(fusca.marca);
-		System.out.println(fusca.modelo);
+    Carro van = new Carro();
+    van.marca = "Fiat";
+    van.modelo = "Ducato";
+    van.numPassageiros = 10;
+    van.capCombustivel = 100;
+    van.consumoCombustivel = 0.2;
 
-    van.exibirAutonomia();
-	}
+    // System.out.println(van.marca);
+    // System.out.println(van.modelo);
+
+    // Carro fusca = new Carro();
+    // fusca.marca = "Volkswagen";
+    // fusca.modelo = "Fusca";
+    // fusca.numPassageiros = 4;
+    // fusca.capCombustivel = 30;
+    // fusca.consumoCombustivel = 0.15;
+
+    // System.out.println(fusca.marca);
+    // System.out.println(fusca.modelo);
+
+    // van.exibirAutonomia();
+
+    double autonomia = van.obterAutonomia();
+    System.out.println("A autonomia do carro é: " + autonomia);
+    System.out.println("A autonomia do carro é: " + van.obterAutonomia());
+  }
 }
