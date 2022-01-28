c533f4f3c090a1e4bdfc1d0a9c9433f37fa43ad9
Bruno Oliveira <brunno.oliver7@gmail.com>
2022-01-27 23:04:30 -0300

    POO This

--- bruno-oliveira/Java Basico/6 - Orientacao a Objetos/Carro.java
+++ bruno-oliveira/Java Basico/6 - Orientacao a Objetos/Carro.java
@@ -5,17 +5,42 @@ public class Carro {
   double capCombustivel;
   double consumoCombustivel;
 
-  Carro() {
-    System.out.println("Classe Carro foi instanciada");
-    numPassageiros = 4;
+  // Carro() {
+  // System.out.println("Classe Carro foi instanciada");
+  // numPassageiros = 4;
+  // }
+
+  public Carro() {
+  };
+
+  // Carro(String marca_, String modelo_, int numPassageiros_, double
+  // capCombustivel_, double consumoCombustivel_) {
+  // marca = marca_;
+  // modelo = modelo_;
+  // numPassageiros = numPassageiros_;
+  // capCombustivel = capCombustivel_;
+  // consumoCombustivel = consumoCombustivel_;
+  // }
+
+  public Carro(String marca, String modelo, int numPassageiros,
+      double capCombustivel, double consumoCombustivel) {
+    this.marca = marca;
+    this.modelo = modelo;
+    this.numPassageiros = numPassageiros;
+    this.capCombustivel = capCombustivel;
+    this.consumoCombustivel = consumoCombustivel;
   }
 
-  Carro(String marca_, String modelo_, int numPassageiros_, double capCombustivel_, double consumoCombustivel_) {
-    marca = marca_;
-    modelo = modelo_;
-    numPassageiros = numPassageiros_;
-    capCombustivel = capCombustivel_;
-    consumoCombustivel = consumoCombustivel_;
+  public Carro(String marca, String modelo, int numPassageiros) {
+    this.marca = marca;
+    this.modelo = modelo;
+    this.numPassageiros = numPassageiros;
+    System.out.println("Chamando o construtor com 3 parâmetros");
+  }
+
+  public Carro(String marca, String modelo) {
+    this(marca, modelo, 10);
+    System.out.println("Chamando o construtor com 2 parâmetros");
   }
 
   void exibirAutonomia() {
--- bruno-oliveira/Java Basico/6 - Orientacao a Objetos/TesteCarro.java
+++ bruno-oliveira/Java Basico/6 - Orientacao a Objetos/TesteCarro.java
@@ -1,11 +1,11 @@
 public class TesteCarro {
   public static void main(String[] args) {
-    Carro van = new Carro();
-    van.marca = "Fiat";
-    van.modelo = "Ducato";
-    // van.numPassageiros = 10;
-    van.capCombustivel = 100;
-    van.consumoCombustivel = 0.2;
+    // Carro van = new Carro();
+    // van.marca = "Fiat";
+    // van.modelo = "Ducato";
+    // // van.numPassageiros = 10;
+    // van.capCombustivel = 100;
+    // van.consumoCombustivel = 0.2;
 
     // System.out.println(van.marca);
     // System.out.println(van.modelo);
@@ -32,16 +32,20 @@ public class TesteCarro {
 		// System.out.println("qtdCombustivel10 = " + qtdCombustivel10);
 		// System.out.println("qtdCombustivel15 = " + qtdCombustivel15);
 
-    System.out.println(van.numPassageiros);
+    // System.out.println(van.numPassageiros);
 		
-		Carro van2 = new Carro("Fiat", "Ducato", 10, 100, 0.2);
+		// Carro van2 = new Carro("Fiat", "Ducato", 10, 100, 0.2);
 		
-		System.out.println(van2.marca);
-		System.out.println(van2.modelo);
-		System.out.println(van2.numPassageiros);
-		System.out.println(van2.capCombustivel);
-		System.out.println(van2.consumoCombustivel);
+		// System.out.println(van2.marca);
+		// System.out.println(van2.modelo);
+		// System.out.println(van2.numPassageiros);
+		// System.out.println(van2.capCombustivel);
+		// System.out.println(van2.consumoCombustivel);
 		
-		Carro2 carro2 = new Carro2();
+		// Carro2 carro2 = new Carro2();
+
+    Carro van = new Carro("Fiat", "Ducato");
+
+		System.out.println(van.numPassageiros);
   }
 }
